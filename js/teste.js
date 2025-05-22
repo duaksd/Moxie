1cc5872d2865015d81cc6c8998c193350ac1dd4a
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-05-22 17:18:35 -0300

    update da pagina produtos 😎🥶

--- js/teste.js
+++ js/teste.js
@@ -2,25 +2,27 @@ const produtos = [
     {
         imagem: "img/plus-size.jpeg",
         alt: "Conjunto Fitness Plus Size Preto",
-        titulo: "Conjunto Fitness Plus Size Preto com Detalhes Listrados",
+        nome: "Conjunto Fitness Plus Size Preto com Detalhes Listrados",
         descricao: "Conjunto de treino feminino de alta elasticidade, desenvolvido especialmente para conforto e estilo. O top de alças largas oferece suporte seguro, enquanto a legging de cintura alta valoriza a silhueta, com detalhes listrados que adicionam um toque de modernidade. Ideal para atividades físicas, caminhadas ou uso casual esportivo. Confeccionado com tecido respirável e de secagem rápida, perfeito para o seu dia a dia ativo.",
         preco: "R$ 115,99",
         parcelamento: "3x de R$ 38,66",
-        tamanho: "gg",
+        tamanho: "GG",
     },
     {
         imagem: "img/fem1.png",
         alt: "Shorts Fitness Feminino Moxie",
-        titulo: "Shorts Fitness Feminino Moxie",
+        nome: "Shorts Fitness Feminino Moxie",
         descricao: "Shorts leve e confortável da linha Moxie, desenvolvido para atividades físicas intensas ou leisurely. Confeccionado com tecido de alta respirabilidade e elasticidade, garantindo liberdade de movimento e secagem rápida. Design estiloso na cor coral, com cintura elástica para melhor ajuste e conforto durante o uso.",
         preco: "R$ 78,89",
         parcelamento: "2x de R$ 39,45",
-        tamanho: "p",
+        tamanho: "P",
+        genero: "feminino",
+        cor: "coral",
     },
     {
         imagem: "img/munhequeira.jpeg",
         alt: "Munhequeira PowerGrip (Par)",
-        titulo: "Munhequeira PowerGrip (Par)",
+        nome: "Munhequeira PowerGrip (Par)",
         descricao: "Estabilidade e proteção em cada repetição. A Munhequeira PowerGrip foi desenvolvida para dar suporte aos punhos durante treinos de musculação, crossfit e levantamento de peso. Com ajuste em velcro e tecido elástico respirável, ela previne lesões sem limitar seus movimentos. Ideal para quem leva o treino a sério.",
         preco: "R$ 39,99",
         parcelamento: "2x de R$ 20,00",
@@ -28,7 +30,7 @@ const produtos = [
     {
         imagem: "img/mae-roupa.jpeg",
         alt: "Conjunto Fitness Mãe Atleta",
-        titulo: "Conjunto Fitness Mãe Atleta",
+        nome: "Conjunto Fitness Mãe Atleta",
         descricao: "Este conjunto feminino de roupas de fitness foi criado para celebrar a força, energia e conexão entre mães e seus filhos. Com tecido dry-fit leve, que proporciona conforto e liberdade de movimento, é perfeito para treinos em casa, na academia ou ao ar livre. O design moderno e estiloso incentiva uma rotina saudável e cheia de amor.",
         preco: "R$ 119,90",
         parcelamento: "3x de R$ 39,97"
@@ -36,7 +38,7 @@ const produtos = [
     {
         imagem: "img/camisa-preta.jpeg",
         alt: "Camiseta Esportiva Funcional Masculina",
-        titulo: "Camiseta Esportiva Funcional Masculina",
+        nome: "Camiseta Esportiva Funcional Masculina",
         descricao: "Camiseta esportiva de tecido leve e respirável, com tecnologia de absorção de umidade, ideal para treinos na academia. Design moderno e confortável, com ajuste perfeito ao corpo e detalhes refletivos para maior segurança. Perfeita para quem busca desempenho e estilo durante as atividades físicas.",
         preco: "R$ 89,90",
         parcelamento: "2x de R$ 44,95"
@@ -44,7 +46,7 @@ const produtos = [
     {
         imagem: "img/fem2.png",
         alt: "Conjunto Fitness Feminino",
-        titulo: "Conjunto Fitness Feminino Lana",
+        nome: "Conjunto Fitness Feminino Lana",
         descricao: "Conjunto de top e legging fitness feitos em tecido de alta performance, com acabamento confortável e resistente. O top oferece suporte perfeito para treinos de alta intensidade, enquanto a legging garante liberdade de movimento e ajuste perfeito ao corpo. Ideal para atividades na academia, yoga ou corrida, proporcionando estilo e conforto.",
         preco: "R$ 149,99",
         parcelamento: "3x de R$ 50,00"
@@ -52,7 +54,7 @@ const produtos = [
     {
         imagem: "img/camisa-longa.jpeg",
         alt: "Camisa de Manga Longa",
-        titulo: "Camisa de Manga Longa",
+        nome: "Camisa de Manga Longa",
         descricao: "Camisa de manga longa feita com tecido técnico de alta performance, ideal para treinos intensos e atividades ao ar livre. Possui design ajustado, respirável e com tecido que ajuda na gestão da umidade, proporcionando conforto e liberdade de movimento durante o exercício.",
         preco: "R$ 86,90",
         parcelamento: "2x de R$ 43,45"
@@ -60,7 +62,7 @@ const produtos = [
     {
         imagem: "img/alta-performance.jpeg",
         alt: "Camiseta masculina de alta performance",
-        titulo: "Camiseta masculina de alta performance",
+        nome: "Camiseta masculina de alta performance",
         descricao: "Camiseta masculina de alta performance, feita com tecido respirável, leve e elasticidade que acompanha seus movimentos durante os treinos. Design slim fit, ideal para quem busca conforto e estilo na academia ou na rotina esportiva. Detalhes modernos com acabamento de alta qualidade, perfeita para quem leva a sério sua performance.",
         preco: "R$ 98,90",
         parcelamento: "2x de R$ 49,45"
@@ -68,7 +70,7 @@ const produtos = [
     {
         imagem: "img/camisa-preta2.jpg",
         alt: "Camisa Preta Justa",
-        titulo: "Camisa Preta Justa",
+        nome: "Camisa Preta Justa",
         descricao: "Camisa de compressão masculina de alta performance, confeccionada com tecido técnico de excelência que garante máxima respirabilidade, excelente elasticidade e suporte firme. Ideal para treinos intensos, corridas ou atividades que exigem rendimento máximo, proporcionando conforto e estabilidade durante toda a performance.",
         preco: "R$ 94,90",
         parcelamento: "2x de R$ 47,45"
@@ -76,7 +78,7 @@ const produtos = [
     {
         imagem: "img/casal-conjunto.jpeg",
         alt: "Conjunto Casual para Casal",
-        titulo: "Conjunto Casual para Casal",
+        nome: "Conjunto Casual para Casal",
         descricao: "Conjunto de roupas fitness para casal, combina estilo e conforto. Tecido respirável, ótimo para treinos na academia ou ao ar livre. Disponível em várias cores e tamanhos.",
         preco: "R$ 199,90",
         parcelamento: "4x de R$ 49,98"
@@ -84,7 +86,7 @@ const produtos = [
     {
         imagem: "img/elastico.jpeg",
         alt: "Elástico de Resistência para Exercícios",
-        titulo: "Elástico de Resistência para Exercícios",
+        nome: "Elástico de Resistência para Exercícios",
         descricao: "Elástico de resistência de alta qualidade, ideal para treinos de força, alongamento e reabilitação. Confeito em material durável e flexível, disponível em diferentes níveis de intensidade para incrementar suas sessões de treino. Compacto e fácil de transportar, perfeito para usar em casa, no parque ou na academia.",
         preco: "R$ 49,90",
         parcelamento: "2x de R$ 24,95"
@@ -92,7 +94,7 @@ const produtos = [
     {
         imagem: "img/fem5.png",
         alt: "Macacão Street Moxie",
-        titulo: "Macacão Fit Street Moxie",
+        nome: "Macacão Fit Street Moxie",
         descricao: "Macacão feminino da linha Street Moxie, com design slim e acabamento em tecido stretch de alta resistência. Perfeito para um visual urbano, confortável e moderno, ideal para o dia a dia, treinos ou looks casuais. Cor preta, estilosa e versátil para quem busca praticidade com atitude.",
         preco: "R$ 179,90",
         parcelamento: "3x de R$ 59,97"
@@ -100,21 +102,21 @@ const produtos = [
     /*{
         imagem: "img/verde-jade.jpeg",
         alt: "Conjunto Fitness Verde Jade",
-        titulo: "Conjunto Fitness Verde Jade",
+        nome: "Conjunto Fitness Verde Jade",
         descricao: "Conjunto de roupa fitness composto por top e legging na cor verde jade vibrante. Feito com tecido de alta compressão, que oferece conforto, suporte e liberdade de movimento durante os exercícios. Design moderno e ajustado ao corpo, ideal para treinos na academia, corrida ou yoga. Perfeito para quem busca estilo e funcionalidade na hora de se exercitar.",
         preco: "R$ 129,90"
     },
     {
         imagem: "img/kit-portatil.jpeg",
         alt: "Kit de Treino Portátil para Fitness",
-        titulo: "Kit de Treino Portátil para Fitness",
+        nome: "Kit de Treino Portátil para Fitness",
         descricao: "Leve seu treino para qualquer lugar com este kit completo de exercícios. Inclui um par de halteres ajustáveis, uma garrafa de água para hidratação, uma esteira dobrável e uma bolsa prática para transporte. Ideal para quem busca manter a rotina de exercícios em casa, na academia ou ao ar livre. Durável, fácil de transportar e perfeito para todos os níveis de condicionamento físico.",
         preco: "R$ 220,99"
     },
     {
         imagem: "img/masc-casual1.jpeg",
         alt: "Conjunto Esportivo Casual",
-        titulo: "Conjunto Esportivo Casual",
+        nome: "Conjunto Esportivo Casual",
         descricao: "Conjunto de camiseta e shorts super finos + camisa moletom ideal para treinos e atividades físicas. O tecido leve e respirável proporciona conforto e liberdade de movimento. Design moderno e estiloso, perfeito para quem busca praticidade e conforto no dia a dia. Disponível em várias cores.",
         preco: "R$ 144,90"
     }*/
@@ -135,7 +137,7 @@ produtos.forEach((produto, index) => {
     const produtoHTML = `
         <div class="produto">
             <img src="${produto.imagem}" alt="${produto.alt}">
-            <h2>${produto.titulo}</h2>
+            <h2>${produto.nome}</h2>
             <p>${descricaoCurta}</p> <!-- Exibe a descrição curta -->
             <p class="preco">${produto.preco}</p>
             <button onclick="redirecionarParaProduto(${index})">Comprar</button>
@@ -147,7 +149,7 @@ produtos.forEach((produto, index) => {
 // Função para redirecionar para a página do produto
 function redirecionarParaProduto(index) {
     const produto = produtos[index];
-    const queryString = `?imagem=${encodeURIComponent(produto.imagem)}&alt=${encodeURIComponent(produto.alt)}&titulo=${encodeURIComponent(produto.titulo)}&descricao=${encodeURIComponent(produto.descricao)}&preco=${encodeURIComponent(produto.preco)}&parcelamento=${encodeURIComponent(produto.parcelamento || '')}&tamanho=${encodeURIComponent(produto.tamanho || '')}`;
+    const queryString = `?imagem=${encodeURIComponent(produto.imagem)}&alt=${encodeURIComponent(produto.alt)}&nome=${encodeURIComponent(produto.nome)}&descricao=${encodeURIComponent(produto.descricao)}&preco=${encodeURIComponent(produto.preco)}&parcelamento=${encodeURIComponent(produto.parcelamento || '')}&tamanho=${encodeURIComponent(produto.tamanho || '')}`;
     window.location.href = `produto.html${queryString}`;
 }
 
