4fb0912406f70b0d30cf5f3dfb12a6caef7185eb
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-06-02 14:38:11 -0300

    att

--- js/TTeste.js
+++ js/TTeste.js
@@ -29,22 +29,22 @@ async function login() {
 }
 
 
-    // function login() {
-    //   const email = document.getElementById('email').value.trim();
-    //   const password = document.getElementById('password').value.trim();
+    function login() {
+      const email = document.getElementById('email').value.trim();
+      const password = document.getElementById('password').value.trim();
 
-    //   const savedEmail = localStorage.getItem('userEmail');
-    //   const savedPassword = localStorage.getItem('userPassword');
+      const savedEmail = localStorage.getItem('userEmail');
+      const savedPassword = localStorage.getItem('userPassword');
 
-    //   if (!email || !password) {
-    //     alert('Preencha todos os campos.');
-    //     return;
-    //   }
+      if (!email || !password) {
+        alert('Preencha todos os campos.');
+        return;
+      }
 
-    //   if (email === savedEmail && password === savedPassword) {
-    //     localStorage.setItem('isLoggedIn', 'true');
-    //     window.location.href = 'usuariologado.html'; //redireciona para a página inicial após o login
-    //   } else {
-    //     alert('Email ou senha incorretos.');
-    //   }
-    // }
+      if (email === savedEmail && password === savedPassword) {
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'usuariologado.html'; //redireciona para a página inicial após o login
+      } else {
+        alert('Email ou senha incorretos.');
+      }
+    }
